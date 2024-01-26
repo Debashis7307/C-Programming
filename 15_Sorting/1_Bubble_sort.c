8d895f4a72a173f0a957190d8c360501ab871226
Debashis7307 <debashisalak@gmail.com>
2024-01-27 00:31:11 +0530

    modification

--- 15_Sorting/1_Bubble_sort.c
+++ 15_Sorting/1_Bubble_sort.c
@@ -4,11 +4,20 @@ int main(){
     int a;
     printf("Enter the size of the list : ");
     scanf("%d",&a);
+
+    //inecealizing an array
+
     int arr[a];
+
+    //input a list of elements
+
     printf("\nEnter the elements of the list : ");
     for(int i=0;i<a;i++){
         scanf("%d",&arr[i]);
     }
+
+    // Bubble sort
+
     for(int i=0;i<a;i++){
         for(int j=0;j<a-1-i;j++){
             if(arr[j]>arr[j+1]){
@@ -18,6 +27,9 @@ int main(){
             }
         }
     }
+
+    //print the output
+
     printf("\nNow the sorted list is: \n");
     for(int i=0;i<a;i++){
         printf("%d ",arr[i]);
