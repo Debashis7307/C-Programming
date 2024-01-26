7b018d4175d9904956bbadcd73a0b57256412ff5
Debashis7307 <debashisalak@gmail.com>
2024-01-27 00:25:34 +0530

    currection

--- 15_Sorting/1_Bubble_sort.c
+++ 15_Sorting/1_Bubble_sort.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 
 int main(){
-    int a,b;
+    int a;
     printf("Enter the size of the list : ");
     scanf("%d",&a);
     int arr[a];
     printf("\nEnter the elements of the list : ");
     for(int i=0;i<a;i++){
-        scanf("%d",arr);
+        scanf("%d",&arr[i]);
     }
     for(int i=0;i<a;i++){
-        for(int j=0;j<a-i;j++){
+        for(int j=0;j<a-1-i;j++){
             if(arr[j]>arr[j+1]){
-                b = arr[j+1];
-                arr[j+1]= arr[j];
-                arr[j]=b;
+                int tem = arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1]= tem;
             }
         }
     }
     printf("\nNow the sorted list is: \n");
     for(int i=0;i<a;i++){
-        printf("%d",arr);
+        printf("%d ",arr[i]);
     }
     return 0;
 }
